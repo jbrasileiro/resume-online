340deef637f51aa1b9fd79d62c95755dc2226629
João Carlos Brasileiro <jcbrasileiro@hotmail.com>
2019-06-03 20:42:31 -0300

    fix PMD issue : https://pmd.github.io/pmd-5.6.1/pmd-java/rules/java/empty.html#EmptyStatementNotInLoop

--- resume-online-frontend/resume-online-jfx-core/src/main/java/resumeonline/jfx/core/ItemTableView.java
+++ resume-online-frontend/resume-online-jfx-core/src/main/java/resumeonline/jfx/core/ItemTableView.java
@@ -4,36 +4,35 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Button;
 
 public class ItemTableView {
-	
+
 	private SimpleStringProperty nomeArquivo;
-	
 	private SimpleStringProperty endereco;
-	
 	private Button btnRemover;
 
 	public String getNomeArquivo() {
 		return nomeArquivo.get();
 	}
 
-	public void setNomeArquivo(String nomeArquivo) {
-		this.nomeArquivo = new SimpleStringProperty(nomeArquivo);;
+	public void setNomeArquivo(
+		final String nomeArquivo) {
+		this.nomeArquivo = new SimpleStringProperty(nomeArquivo);
 	}
 
 	public String getEndereco() {
 		return endereco.get();
 	}
 
-	public void setEndereco(String endereco) {
-		this.endereco = new SimpleStringProperty(endereco);;
+	public void setEndereco(
+		final String endereco) {
+		this.endereco = new SimpleStringProperty(endereco);
 	}
 
 	public Button getBtnRemover() {
 		return btnRemover;
 	}
 
-	public void setBtnRemover(Button btnRemover) {
+	public void setBtnRemover(
+		final Button btnRemover) {
 		this.btnRemover = btnRemover;
 	}
-	
-
 }
